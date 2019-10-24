2b40a7c78fc25efbcf457b2dfb6924ab8308c457
Superdref <frederiquemendy2205@gmail.com>
2019-10-24 13:20:00 +0200

    Header Ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from "./Header";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">Fred</div>
+     <div className="container">
+      <Header /> 
+     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Who.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Who = ({name, job}) => {
+    return (
+        <div className="col-9">
+                <h1>name</h1>
+                <h2>job</h2>
+            </div>
+    );
+};
+
+export default Who;
