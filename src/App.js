b2b7007eadefc56a160216a8ccf704b920e00c8c
Superdref <frederiquemendy2205@gmail.com>
2019-10-29 15:08:24 +0100

    Changemnt de branche sans écrasement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Header from "./Header";
+import Header from './Header';
+{/*import Intro from './Intro';*/}
+import Footer from './Footer';
+
 
 const App = () => {
   return (
-     <div className="container">
-      <Header /> 
-     </div>
+    <div className="container">
+      <Header />
+      <hr />
+      <Intro image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJBpQrUIqfnOA0oVQFT9tqbT_hpro7ezvYdh8WWJCp08rWbcDF&s" />
+      <hr/>
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <footer className="row">
+            <ul className="d-flex flex-row list-unstyled justify-content-around">
+                <li className="">Github</li>
+                <li>Linkedin</li>
+                <li>Instagram</li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
